3c455a120efcc827fb9c75ca871b14aa792f8497
Chris Bassano <christo4b@gmail.com>
2017-02-08 11:21:48 -0800

    modularized conversion function, api secrets

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const fs = require('fs')
 const https = require('https')
 const es = require('event-stream')
 const readLine = require('readline')
-const timeStamp = require('unix-timestamp')
 const querystring = require('querystring')
-timeStamp.round = true
-
+const convertAmp = require('./src/conversion.js')
+const secret = require('./secret.js')
 const pathToFile = './30events.txt'
 const options = {
   hostname: 'api.amplitude.com',
@@ -22,7 +21,7 @@ fs.createReadStream(pathToFile)
     line = JSON.stringify(convertAmp(line))
 
     var post_data = querystring.stringify({
-      api_key:'b8a4b65d5b20fca2615bcae6154ef56e',
+      api_key: secret.api_key,
       event:line
     })
     
@@ -50,30 +49,5 @@ fs.createReadStream(pathToFile)
   }))
 
 
-function convertAmp(line) {
-  line = JSON.parse(line)
 
-  //INSERT ERROR CHECKING __ 
-    // Do they have userid or device id? what about event type?
-
-  // UNIT TESTING important for this
-
-  let ret = {
-    user_id: line.userId,
-    device_id: line.context.device.id,
-    time: timeStamp.fromDate(line.timestamp),
-    os_name: line.context.os.name,
-    os_version:line.context.os.version,
-    platform: line.context.device.type,
-    event_type: line.type,
-    location_lat: line.context.location.latitude,
-    location_lng: line.context.location.longitude,
-    ip: line.ip,
-    carrier: line.context.network.carrier,
-    device_model: line.context.device.model,
-    device_brand: line.context.device.brand,
-  }
-
-  return ret
-}
 
---
+++ src/conversion.js
@@ -0,0 +1,31 @@
+const timeStamp = require('unix-timestamp')
+timeStamp.round = true
+
+const convertAmp = (line) => {
+  line = JSON.parse(line)
+
+  //INSERT ERROR CHECKING __ 
+    // Do they have userid or device id? what about event type?
+
+  // UNIT TESTING important for this
+
+  let ret = {
+    user_id: line.userId,
+    device_id: line.context.device.id,
+    time: timeStamp.fromDate(line.timestamp),
+    os_name: line.context.os.name,
+    os_version:line.context.os.version,
+    platform: line.context.device.type,
+    event_type: line.type,
+    location_lat: line.context.location.latitude,
+    location_lng: line.context.location.longitude,
+    ip: line.ip,
+    carrier: line.context.network.carrier,
+    device_model: line.context.device.model,
+    device_brand: line.context.device.brand,
+  }
+
+  return ret
+}
+
+module.exports = convertAmp
